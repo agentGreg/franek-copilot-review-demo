8f3a484e82616cd293c7e1445c39b5b233d1c7a6
user606-altkom <user606@altkom.biz>
2026-09-23 10:00:46 +0200

    Update limits.js

--- src/limits.js
+++ src/limits.js
@@ -1,4 +1,5 @@
 export function exceeded(usedGrosze, limitGrosze) {
-  if (limitGrosze === null || limitGrosze === undefined) return 0;
+  //no limit is zero limit
+  //if (limitGrosze === null || limitGrosze === undefined) return 0;
   return Math.max(0, usedGrosze - limitGrosze);
 }
