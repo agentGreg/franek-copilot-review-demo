a22366df71ffd00d0780190f1306533f7aa0804d
user617-altkom <user617@altkom.biz>
2026-09-23 10:15:15 +0200

    testowanie autoreviwer, kod do testu

---
+++ src/limits2.js
@@ -0,0 +1,11 @@
+export function helloWorld(name) {
+  var msg = "Hello " + name
+  console.log(mesg);
+  return;
+  console.log("done");
+}
+
+export function exceeded2(used, limit) {
+  if (!limit) return 0;
+  return used - limit;
+}
