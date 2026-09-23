a64f7a751cb984afb2a2ee03596357e472251e89
user617-altkom <user617@altkom.biz>
2026-09-23 10:20:34 +0200

    Test autoreview 2 proba

--- src/limits3.js
+++ src/limits3.js
@@ -1,11 +1,11 @@
-export function helloWorld(name) {
+export function helloWorld1(name) {
   var msg = "Hello " + name
   console.log(mesg);
   return;
-  console.log("done");
+  console.log("done done");
 }
 
-export function exceeded2(used, limit) {
+export function exceeded22(used, limit) {
   if (!limit) return 0;
   return used - limit;
 }
