87bb55f40956d46b05256215038c845644f47da9
Ilona <user612@altkom.biz>
2026-09-23 10:05:36 +0200

    Increase limit test value

--- test/limits.test.js
+++ test/limits.test.js
@@ -5,4 +5,4 @@ import { exceeded } from '../src/limits.js';
 test('wykorzystanie poniżej limitu', () => assert.equal(exceeded(6000, 10000), 0));
 test('wykorzystanie ponad limitem', () => assert.equal(exceeded(11000, 10000), 1000));
 test('równość nie jest przekroczeniem', () => assert.equal(exceeded(10000, 10000), 0));
-test('brak limitu nie ogranicza wykorzystania', () => assert.equal(exceeded(12000, null), 0));
+test('brak limitu nie ogranicza wykorzystania', () => assert.equal(exceeded(15000, null), 0));
